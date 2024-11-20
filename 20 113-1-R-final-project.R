f24f0dedbef4c611f33dd612df91035377540557
fred432cf <fred432cf@gmail.com>
2024-11-20 09:24:19 +0000

    :(

---
+++ 20 113-1-R-final-project.R
@@ -0,0 +1,16 @@
+library(tidyverse) 
+survey <- read_csv("金融檢查執行情形.csv") 
+
+glimpse(survey) 
+
+tidy_survey1 <- survey %>% 
+  rename(
+    inspection_type = `檢查性質`,
+    financial_industry = `金融業別`,
+    year = `年度`,
+    institution_count = `家數`
+  )
+
+glimpse(tidy_survey1)
+
+install.packages("remotes")

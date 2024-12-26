cfae057cb788452a4c6f375bea07caf28095efec
fred432cf <fred432cf@gmail.com>
2024-12-26 17:29:22 +0000

    2024/12/27

--- code.R
+++ code.R
@@ -3,23 +3,21 @@ library(lubridate)
 library(dplyr)
 survey <- read_csv("Taichung_City_112_Year_Top_10_Accident_Proned_Roads_1_to_12_Months.csv.csv")
 
-surve <- survey %>% 
-  select(-c(`縣市別代碼`,`市話`, A1, A2, A3))
+survey <- survey %>% 
+  select(-c(`縣市別代碼`,`市話`, 發生時間發生時間 A1, A2, A3))
    
 survey <- survey %>%
-  mutate(`年月` = str_replace_all(`年月`, "年", "-")) %>%   
-  mutate(`年月` = str_replace_all(`年月`, "月", "")) %>%  
-  mutate(`年月` = str_remove_all(`年月`, "-$")) %>% 
-  mutate(`年月` = ym(`年月`) + years(1911))
-unique(survey$`年月`)
+  mutate(
+    `年月` = str_remove_all(`年月`, "-$"),
+    `年月` = str_trim(`年月`),
+    `年月` = str_replace(`年月`, "^\\d{3}", ~ as.numeric(.x) + 1911),
+    `年月` = ym(`年月`),
+    `年月` = floor_date(`年月`, "month"),
+    `年月` = format(`年月`, "%Y-%m")
+  )
 
-year_to_filter <- 2023
-survey_filtered <- survey %>%
-  filter(year(`發生日期`) == year_to_filter)
-
-accident_summary <- survey_filtered %>%
+案件量_by_分局 <- survey %>%
   group_by(`轄區分局`) %>%
-  summarize(`事故數量` = n(), .groups = "drop")
-
-print(accident_summary)
+  summarise(`總件數` = sum(`總件數`, na.rm = TRUE)) %>%
+  arrange(desc(`總件數`))
 
